e3e6bf99ab26446716c64298e372131edeea9a97
SiberAlem <yusuf.kilic7@outlook.com>
2017-04-02 04:10:46 +0300

    Update info.py

--- lib/info.py
+++ lib/info.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*- 
 # infoga - Gathering Email Information Tool
-# Coded by M0M0 (m4ll0k)
+# Coded by M0M0 (AKBATUN)
 
 __info__ = 'Gathering Email Information'
-__author__ = 'M@M@ (M4ll0k)'
+__author__ = 'M@M@ (AKBATUN)'
 __version__ = 'v3.0'
 __site__  = '@h4ckm0'
 __codename__ = 'Sn4d0'
 __name__  = 'Infoga'
-__giturl__ = "https://github.com/m4ll0k/infoga"
+__giturl__ = "https://github.com/SiberAlem/infoga"
 
 
